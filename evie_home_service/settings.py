04cdc5bcec0be3cea2cfb7ee382282bfd5bbb005
eviehomeservice <angelahopekai@gmail.com>
2025-05-23 23:43:28 -0400

    Update settings.py

--- evie_home_service/settings.py
+++ evie_home_service/settings.py
@@ -64,8 +64,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
