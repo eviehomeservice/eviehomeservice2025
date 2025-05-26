f5650d51d6fd483bf69fc39613ceb4c5637138f1
eviehomeservice <angelahopekai@gmail.com>
2025-05-26 09:23:53 -0400

    Update models.py

--- service_app/models.py
+++ service_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.utils import timezone
+from cloudinary.models import CloudinaryField
 
 class ServiceCategory(models.Model):
     name = models.CharField(max_length=100)
